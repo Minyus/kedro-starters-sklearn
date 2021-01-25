3c8a258de43ccf150b89c2f4c4942a776c0a8c9b
Minyus <yusuke.minami86@gmail.com>
2021-01-25 13:28:41 +0000

    Upload log files to MLflow

--- sklearn-mlflow-yamlholic-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/mlflow/mlflow_config.py
+++ sklearn-mlflow-yamlholic-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/mlflow/mlflow_config.py
@@ -14,7 +14,10 @@ mlflow_hooks = (
         filepaths_before_pipeline_run=[
             "conf/base/parameters.yml"
         ],  # Optionally specify the file paths to log before the pipeline runs
-        filepaths_after_pipeline_run=[],  # Optionally specify the file paths to log after the pipeline runs
+        filepaths_after_pipeline_run=[
+            "logs/info.log",
+            "logs/errors.log",
+        ],  # Optionally specify the file paths to log after the pipeline runs
     ),
     pipelinex.MLflowEnvVarsLoggerHook(
         param_env_vars=[
