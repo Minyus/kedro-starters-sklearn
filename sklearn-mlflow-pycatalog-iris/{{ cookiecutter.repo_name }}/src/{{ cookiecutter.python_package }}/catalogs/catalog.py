c4b80db7fba9e589f6968a9b758be427f90389d9
Minyus <yusuke.minami86@gmail.com>
2021-01-25 14:01:51 +0800

    Update comments

--- sklearn-mlflow-pycatalog-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/catalogs/catalog.py
+++ sklearn-mlflow-pycatalog-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/catalogs/catalog.py
@@ -1,17 +1,23 @@
-""" DataSet objects can be defined here in addition to YAML (conf/base/catalog.yml) """
+""" DataSet objects can be defined here in addition to YAML ('conf/base/catalog.yml') """
 
 from kedro.extras.datasets.pandas import CSVDataSet
-from kedro.extras.datasets.pickle import PickleDataSet
 from pipelinex import MLflowDataSet
-
+"""
+[MLflowDataSet]
+if `dataset` arg is:
+- {"pkl", "txt", "yaml", "yml", "json", "csv", "xls", "parquet", "png", "jpeg", "jpg"}: log as an MLflow artifact
+- "m": log as an MLflow metric (numeric)
+- "p": log as an MLflow param (string)
+"""
 
 catalog_dict = {
     "train_df": CSVDataSet(
-        filepath="data/01_raw/train.csv",
+        filepath="data/01_raw/train.csv",  # Read a csv file
     ),
     "test_df": CSVDataSet(
-        filepath="data/01_raw/test.csv",
+        filepath="data/01_raw/test.csv",  # Read a csv file
     ),
-    "model": MLflowDataSet(dataset="pkl"),
-    "pred_df": MLflowDataSet(dataset="csv"),
+    "model": MLflowDataSet(dataset="pkl"),  # Write a pickle file & upload to MLflow
+    "pred_df": MLflowDataSet(dataset="csv"),  # Write a csv file & upload to MLflow
+    "score": MLflowDataSet(dataset="m"),  # Write an MLflow metric
 }
